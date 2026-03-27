a0267f3ab8e957704801c3c2130b2f707f12702d
Mohamed <ouzammad.mohamed@gmail.com>
2026-03-27 15:10:52 +0100

    green test

--- reservation.js
+++ reservation.js
@@ -1,8 +1,33 @@
-export function createReservation(reservation, reservations) {
+export function createReservation(res, reservations) {
+
+  if (res.end <= res.start) {
+    throw new Error("Invalid dates");
+  }
+
+  for (let r of reservations) {
+    if (res.start < r.end && res.end > r.start) {
+      throw new Error("Overlap");
+    }
+  }
+
+  reservations.push(res);
+  return reservations;
 }
 
 export function cancelReservation(id, now, reservations) {
+  const res = reservations.find(r => r.id === id);
+
+  const diff = res.start - now;
+
+  if (diff < 172800000) {
+    throw new Error("Too late");
+  }
+
+  return reservations.filter(r => r.id !== id);
 }
 
 export function getActiveReservations(date, reservations) {
+  return reservations.filter(r =>
+    date >= r.start && date < r.end
+  );
 }
